7f4042d85251c3f64fd0e04e9ca6fe0a3af7363f
legenDehry <shivamshukla292@gmail.com>
2020-09-06 22:41:54 +0530

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,6 +14,6 @@ public class Greeter {
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
-    return String.format("Hello, %s!", someone);
+    return String.format("Hello shivam and you are not, %s!", someone);
   }
 }
